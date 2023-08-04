10b7bc9bc647e93f14bd20c84dde4e0ca486dd45
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-04 01:31:30 +0100

    🚀 Added a smooth-scrolling Scroll to Top button with hover animations and a tooltip 🎈

--- src/components/SecondSection.svelte
+++ src/components/SecondSection.svelte
@@ -2,10 +2,32 @@
 	import About from './sections/About.svelte';
 	import TechStack from './sections/TechStack.svelte';
 	import Project from './sections/Project.svelte';
+	import { ArrowUpIcon } from 'lucide-svelte';
+
+	let showArrow = false;
+
+	window.addEventListener('scroll', () => {
+		const threshold = 250;
+		showArrow = window.scrollY > threshold;
+	});
+
+	function scrollToTop() {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
 </script>
 
 <section class="pt-10 lg:pt-24 lg:w-1/2 lg:py-24 font-space">
 	<About />
 	<TechStack />
 	<Project />
+
+	{#if showArrow}
+		<button
+			title="Scroll to top"
+			class="fixed bottom-0 right-0 mb-4 mr-4 p-2 rounded-full bg-white shadow-lg hover:scale-110 transition duration-300"
+			on:click={scrollToTop}
+		>
+			<ArrowUpIcon size={18} />
+		</button>
+	{/if}
 </section>
