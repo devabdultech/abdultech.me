e1617f5fad2727b2dcdb9402d6438c1ca9017df5
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-04 01:41:36 +0100

    ✨ Added pulse animation to button

--- src/components/ui/Button.svelte
+++ src/components/ui/Button.svelte
@@ -2,8 +2,8 @@
 	class="flex items-center gap-3 mt-4 bg-gradient-to-r from-[#122D22] via-[#265842] to-[#122D22] p-2 w-[250px] lg:px-4 rounded-full"
 >
 	<div class="w-5 h-5 rounded-full bg-[#3ECF8E26] flex justify-center items-center">
-		<div class="w-3 h-3 m-auto rounded-full bg-[rgb(62,207,142)]" />
+		<div class="w-3 h-3 m-auto rounded-full bg-[rgb(62,207,142)] animate-ping" />
 	</div>
 
-	<span class="text-[#3ECF8E]">Available for projects</span>
+	<span class="text-[#3ECF8E] animate-pulse">Available for projects</span>
 </button>
