93c303fd0b291449d563b80fa99f3a779aa4b8b0
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-06 23:03:30 +0100

    ✨Blog page (list) done + 🖌 code format

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,22 +1,110 @@
+<script>
+	import { ArrowUpRightIcon, DotIcon } from 'lucide-svelte';
+</script>
+
 <svelte:head>
 	<title>Blog - AbdulTech</title>
 </svelte:head>
 
-<div class="flex max-w-5xl flex-col gap-5">
+<div class="flex max-w-5xl flex-col gap-4">
 	<div class="flex flex-col gap-3">
-		<h1 class="font-inter text-xl font-[500] md:text-2xl lg:text-2xl">
-			Explore the Cosmos of Thoughts
-		</h1>
+		<h1 class="font-inter text-xl font-[500] md:text-2xl lg:text-2xl">Blog/Thoughts</h1>
 		<div class="font-space flex flex-col gap-1 text-base md:text-lg">
 			<p>
-				Journey through constellations of thoughts and immersive experiences. Witness the dance of
-				ideas and the convergence of passions as we delve into uncharted realms of exploration and
-				creativity.
+				Enter my world of thoughts and experiences, where ideas twirl with visions, and passions
+				come together in an eclectic exploration. Join me on this journey into the realm of
+				curiosity and discovery.
 			</p>
 			<p>
-				<span class="text-textAccent">Why embark on this cosmic odyssey?</span> While the reason eludes
-				me, I extend this celestial invitation to you nonetheless.
+				<span class="text-textAccent">Why are we embarking on this? </span> I can't quite put my finger
+				on it, but here's an invitation nonetheless :}
 			</p>
+		</div>
+	</div>
+
+	<div class="mt-6 flex flex-col gap-6">
+		<div class="flex w-full items-center gap-3">
+			<div class="flex h-24 w-24 flex-col items-center justify-center rounded-full bg-teal-600 p-5">
+				<h1 class="text-xl font-bold">02</h1>
+				<p class="text-center text-sm font-medium text-textPrimary">September, 2023</p>
+			</div>
+			<div class="relative w-5/6 border-l border-l-emerald-500 py-3 pl-4">
+				<div class="absolute -left-1.5 top-0 aspect-square w-3 rounded-full bg-textAccent" />
+				<a href="/blog/my-first-blog" class="group transition-all">
+					<div class="flex gap-2 text-xl font-semibold transition-transform">
+						<span>My first blog :)</span>
+						<ArrowUpRightIcon
+							size={16}
+							class="transform duration-200 group-hover:translate-x-1 group-hover:translate-y-[-2px]"
+						/>
+						<span class="flex items-center text-xs text-textPrimary/50"><DotIcon /> 10min</span>
+					</div>
+					<p
+						class="mt-2 max-w-xs truncate text-sm font-medium leading-relaxed text-textPrimary/70 lg:mt-1"
+					>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ad cupiditate provident
+						fugit, odio deleniti recusandae soluta nihil temporibus nemo, esse minus enim a sint
+						voluptates, corrupti commodi magni illum.
+					</p>
+					<div class="absolute -left-1.5 bottom-0 aspect-square w-3 rounded-full bg-textAccent" />
+				</a>
+			</div>
+		</div>
+
+		<div class="flex w-full items-center gap-3">
+			<div class="flex h-24 w-24 flex-col items-center justify-center rounded-full bg-teal-600 p-5">
+				<h1 class="text-xl font-bold">09</h1>
+				<p class="text-center text-sm font-medium text-textPrimary">December, 2023</p>
+			</div>
+			<div class="relative w-5/6 border-l border-l-emerald-500 py-3 pl-4">
+				<div class="absolute -left-1.5 top-0 aspect-square w-3 rounded-full bg-textAccent" />
+				<a href="/blog/my-second-blog" class="group transition-all">
+					<div class="flex gap-2 text-xl font-semibold transition-transform">
+						<span>My second blog :)</span>
+						<ArrowUpRightIcon
+							size={16}
+							class="transform duration-200 group-hover:translate-x-1 group-hover:translate-y-[-2px]"
+						/>
+						<span class="flex items-center text-xs text-textPrimary/50"><DotIcon /> 10min</span>
+					</div>
+					<p
+						class="mt-2 max-w-xs truncate text-sm font-medium leading-relaxed text-textPrimary/70 lg:mt-1"
+					>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ad cupiditate provident
+						fugit, odio deleniti recusandae soluta nihil temporibus nemo, esse minus enim a sint
+						voluptates, corrupti commodi magni illum.
+					</p>
+					<div class="absolute -left-1.5 bottom-0 aspect-square w-3 rounded-full bg-textAccent" />
+				</a>
+			</div>
+		</div>
+
+		<div class="flex w-full items-center gap-3">
+			<div class="flex h-24 w-24 flex-col items-center justify-center rounded-full bg-teal-600 p-5">
+				<h1 class="text-xl font-bold">01</h1>
+				<p class="text-center text-sm font-medium text-textPrimary">January, 2024</p>
+			</div>
+			<div class="relative w-5/6 border-l border-l-emerald-500 py-3 pl-4">
+				<div class="absolute -left-1.5 top-0 aspect-square w-3 rounded-full bg-textAccent" />
+				<a href="/blog/my-third-blog" class="group transition-all">
+					<div class="flex gap-2 text-xl font-semibold transition-transform">
+						<span>My third blog :)</span>
+						<ArrowUpRightIcon
+							size={16}
+							class="transform duration-200 group-hover:translate-x-1 group-hover:translate-y-[-2px]"
+						/>
+						<span class="flex items-center text-xs text-textPrimary/50"><DotIcon /> 10min</span>
+					</div>
+					<p
+						class="mt-2 max-w-xs truncate text-sm font-medium leading-relaxed text-textPrimary/70 lg:mt-1"
+					>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ad cupiditate provident
+						fugit, odio deleniti recusandae soluta nihil temporibus nemo, esse minus enim a sint
+						voluptates, corrupti commodi magni illum.
+					</p>
+					<div class="absolute -left-1.5 bottom-0 aspect-square w-3 rounded-full bg-textAccent" />
+				</a>
+			</div>
 		</div>
 	</div>
 </div>
