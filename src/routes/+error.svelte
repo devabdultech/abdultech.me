77108e32f5d5d7df8dc28b1bce627de3e7026450
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-10 09:54:31 +0100

    ❎ 404 page done

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,19 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
 </script>
 
-<div class="error">
-	<h1>{$page.status}: {$page.error?.message}</h1>
-</div>
+<svelte:head>
+	<title>{$page.status}: {$page.error?.message}</title>
+</svelte:head>
 
-<style>
-	.error {
-		height: 100%;
-		display: grid;
-		place-content: center;
-	}
-</style>
+<div class="mx-auto mt-8 flex max-h-screen flex-col items-center justify-center text-textPrimary">
+	<h1 class="font-inter text-3xl font-bold">{$page.status}: {$page.error?.message}</h1>
+	<p class="mt-4 text-xl text-textAccent">Oops page not found. Nice to see you here thou :)</p>
+	<div class="mt-5 max-w-[500px]">
+		<img class="w-full" src="/404-gif.gif" alt="404 Gif" />
+	</div>
+	<p><a href="https://giphy.com/gifs/computer-reddit-bPCwGUF2sKjyE">via GIPHY</a></p>
+	<div class="mt-6">
+		<a class="text-xl font-semibold text-textAccent" href="/"> [Home] </a>
+	</div>
+</div>
