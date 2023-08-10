3b0ff4681c442cd026c79166b4f7c11fa67444f7
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-08-10 10:08:10 +0100

    🎉 Added transition

--- src/components/FirstSection.svelte
+++ src/components/FirstSection.svelte
@@ -3,10 +3,13 @@
 	import SocialCard from './ui/SocialCard.svelte';
 	import Button from './ui/Button.svelte';
 	import Nav from './ui/Nav.svelte';
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 	import SpotifyActivity from './ui/SpotifyActivity.svelte';
 </script>
 
 <section
+	transition:slide={{ delay: 250, duration: 300, easing: quintOut, axis: 'x' }}
 	class="font-space lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24"
 >
 	<div class="flex flex-col gap-3">
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,7 +8,9 @@
 
 <div class="mx-auto mt-8 flex max-h-screen flex-col items-center justify-center text-textPrimary">
 	<h1 class="font-inter text-3xl font-bold">{$page.status}: {$page.error?.message}</h1>
-	<p class="mt-4 text-xl text-textAccent">Oops page not found. Nice to see you here thou :)</p>
+	<p class="mt-4 text-center text-xl text-textAccent">
+		Oops page not found. Nice to see you here thou :)
+	</p>
 	<div class="mt-5 max-w-[500px]">
 		<img class="w-full" src="/404-gif.gif" alt="404 Gif" />
 	</div>
