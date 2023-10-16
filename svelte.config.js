6380ec8955d1d46ba8904594ab159241562fa45b
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-10-16 06:05:42 +0100

    ⚒ fix deployment issue (test)

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { escapeSvelte, mdsvex } from 'mdsvex';
 import shiki from 'shiki';
