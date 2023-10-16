3121e4e763f572520a4ecb8df61bb386a949fabb
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-10-16 20:08:22 +0100

    👦 added a function that calculates my age

--- src/components/sections/About.svelte
+++ src/components/sections/About.svelte
@@ -1,3 +1,20 @@
+<script lang="ts">
+	function calculateAge(dateOfBirth: string) {
+		const dob = new Date(dateOfBirth);
+		const currentDate = new Date();
+		let age = currentDate.getFullYear() - dob.getFullYear();
+		const month = currentDate.getMonth() - dob.getMonth();
+
+		if (month < 0 || (month === 0 && currentDate.getDate() < dob.getDate())) {
+			age--;
+		}
+
+		return age;
+	}
+
+	const myAge = calculateAge('2005-12-09');
+</script>
+
 <div
 	id="about"
 	class="font-space mb-16 scroll-mt-16 text-textPrimary lg:min-h-screen lg:scroll-mt-24"
@@ -10,36 +27,38 @@
 		class="slide-enter-content font-space flex flex-col gap-y-3 text-lg font-[300] leading-relaxed text-textPrimary"
 	>
 		<p>
-			I'm a passionate self-taught developer and a
+			I'm a {myAge} year old self-taught developer currently studying
 			<a
 				href="https://en.wikipedia.org/wiki/Computer_science"
 				target="_blank"
 				class="cursor-laptop text-textAccent transition duration-200 hover:underline"
 			>
-				Computer Science
+				cs
 			</a>
-			student from Nigeria. Building things that run on computers is not only my area of study but also
-			my
-			<span class="text-textAccent">biggest passion.</span>
+		from Nigeria.
 			I love diving into various tools,
 			<a
 				href="https://en.wikipedia.org/wiki/Programming_language"
 				target="_blank"
-				class="text-textAccent">programming languages</a
+				class="text-textAccent"
+				>programming languages</a
 			>
 			and
 			<a
 				href="https://en.wikipedia.org/wiki/Application_framework"
 				target="_blank"
-				class="text-textAccent">frameworks</a
+				class="text-textAccent"
+				>frameworks</a
 			> to expand my knowledge and skills.
 		</p>
 
 		<p>
-			Being a <a
+			Being a
+			<a
 				href="https://en.wikipedia.org/wiki/Software_engineer"
 				target="_blank"
-				class="text-textAccent">developer</a
+				class="text-textAccent"
+				>developer</a
 			>
 			is not just a job for me; it's a way of life. I'm always exploring new ideas and experimenting
 			with different technologies. I always strive to push the boundaries and improve my craft. I aim
@@ -48,18 +67,23 @@
 
 		<p>
 			When the night comes, you'll often find me enthusiastically learning how to write the best
-			software possible and optimizing it to run even better. The thrill of making <a
+			software possible and optimizing it to run even better. The thrill of making
+			<a
 				href="https://en.wikipedia.org/wiki/Computer_software"
 				target="_blank"
-				class="text-textAccent">software</a
-			> more efficient and effective keeps me going.
+				class="text-textAccent"
+				>software</a
+			>
+			more efficient and effective keeps me going.
 		</p>
 
 		<p>
-			Still exploring the world of <a
+			Still exploring the world of
+			<a
 				href="https://en.wikipedia.org/wiki/Open_source"
 				target="_blank"
-				class="text-textAccent">open source,</a
+				class="text-textAccent"
+				>open source,</a
 			> and I'm eager to contribute to cool and helpful projects ✨
 		</p>
 	</div>
