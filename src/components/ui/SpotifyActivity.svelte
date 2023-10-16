8190c3db65e299046b544b413fbe9bb41ba62b9a
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-10-16 22:01:42 +0100

    ➡ testing SSE

--- src/components/ui/SpotifyActivity.svelte
+++ src/components/ui/SpotifyActivity.svelte
@@ -4,54 +4,73 @@
 
 	let data: SpotifyData;
 
-	async function fetchData() {
-		try {
-			const response = await fetch('/api/spotify');
-			if (response.ok) {
-				data = await response.json();
-			} else {
-				console.error('Failed to fetch data:', response.statusText);
-			}
-		} catch (error) {
-			console.error('Error:', error);
-		}
-	}
+	onMount(async () => {
+		const eventSource = new EventSource('/api/spotify');
+		eventSource.onmessage = (event) => {
+			data = JSON.parse(event.data);
+			console.log(data.item);
+		};
 
-	onMount(() => {
-		fetchData();
+		eventSource.onerror = (error) => {
+			console.log('Error receiving SSE stream:', error);
+		};
 	});
 </script>
 
 <div class="text-white">
 	{#if data && data.item}
-		<div class="flex flex-col gap-4 ">
-			<a class="flex items-center justify-between" target="_blank" href="https://open.spotify.com/user/wgrj76kh7jdx0kss4ydqhn12i">
+		<div class="flex flex-col gap-4">
+			<a
+				class="flex items-center justify-between"
+				target="_blank"
+				href="https://open.spotify.com/user/wgrj76kh7jdx0kss4ydqhn12i"
+			>
 				<h3 class="uppercase leading-relaxed">Listening to spotify</h3>
-				<img class="w-8 h-8" src="/spotify.svg" alt="Spotify" />
-				
+				<img class="h-8 w-8" src="/spotify.svg" alt="Spotify" />
 			</a>
 
-			<div class="flex gap-4 w-full h-20">
-				<div class="max-w-[80px] h-full rounded-lg overflow-hidden flex-shrink-0">
-					<img class="w-full h-full object-cover" src={data.item.album.images[0].url} alt="Spotify" />
+			<div class="flex h-20 w-full gap-4">
+				<div class="h-full max-w-[80px] flex-shrink-0 overflow-hidden rounded-lg">
+					<img
+						class="h-full w-full object-cover"
+						src={data.item.album.images[0].url}
+						alt="Spotify"
+					/>
 				</div>
-				
-				<div class="flex flex-col leading-relaxed w-full">
-					<a class="hover:underline font-semibold" href={data.item.external_urls.spotify} target="_blank">{data.item.name}</a>
 
-					<h3>by {#each data.item.artists as artist, index}
-						<a class="hover:underline" href={artist.external_urls.spotify} target="_blank">{artist.name}</a>{#if index < data.item.artists.length - 1}, {/if}
-					{/each}</h3>
-					
-					<h3>on <a class="hover:underline" href={data.item.album.external_urls.spotify} target="_blank">{data.item.album.name}</a></h3>
+				<div class="flex w-full flex-col leading-relaxed">
+					<a
+						class="font-semibold hover:underline"
+						href={data.item.external_urls.spotify}
+						target="_blank">{data.item.name}</a
+					>
+
+					<h3>
+						by {#each data.item.artists as artist, index}
+							<a class="hover:underline" href={artist.external_urls.spotify} target="_blank"
+								>{artist.name}</a
+							>{#if index < data.item.artists.length - 1},
+							{/if}
+						{/each}
+					</h3>
+
+					<h3>
+						on <a
+							class="hover:underline"
+							href={data.item.album.external_urls.spotify}
+							target="_blank">{data.item.album.name}</a
+						>
+					</h3>
 				</div>
 			</div>
-
-			
 		</div>
 	{:else}
-		<a class="flex items-center gap-4" target="_blank" href="https://open.spotify.com/user/wgrj76kh7jdx0kss4ydqhn12i">
-			<img class="w-8 h-8" src="/spotify.svg" alt="Spotify" />
+		<a
+			class="flex items-center gap-4"
+			target="_blank"
+			href="https://open.spotify.com/user/wgrj76kh7jdx0kss4ydqhn12i"
+		>
+			<img class="h-8 w-8" src="/spotify.svg" alt="Spotify" />
 			<h3>Spotify — Not playing</h3>
 		</a>
 	{/if}
--- src/routes/api/spotify/+server.ts
+++ src/routes/api/spotify/+server.ts
@@ -1,16 +1,18 @@
-import { json } from '@sveltejs/kit';
 import { getCurrentlyPlaying } from '$lib/spotify';
 
 export async function GET() {
-	try {
-		const currentlyPlaying = await getCurrentlyPlaying();
+	const stream = new ReadableStream({
+		async start(controller) {
+			const currentlyPlaying = await getCurrentlyPlaying();
+			controller.enqueue(JSON.stringify(currentlyPlaying));
+			controller.close();
+		}
+	});
 
-		return json(currentlyPlaying);
-	} catch (error) {
-		return json({
-			isPlaying: false,
-			error:
-				'Failed to retrieve data from Spotify API. The user is not currently listening to anything.'
-		});
-	}
+	return new Response(stream, {
+		headers: {
+			'Content-Type': 'text/event-stream',
+			'Cache-Control': 'no-cache'
+		}
+	});
 }
