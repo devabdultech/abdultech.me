f4cb9cb774b09caf8c081b2b2e6676206c87bc1e
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-11-12 23:26:07 +0100

    ✨ update OG image template with new design

--- src/routes/api/og/+server.ts
+++ src/routes/api/og/+server.ts
@@ -13,9 +13,33 @@ export const GET: RequestHandler = async () => {
 	}
 
 	const template = html`
-		<div tw="bg-gray-50 flex w-full h-full items-center justify-center">
-			<img tw="w-full bg-black" src="${imagePath}/dystopian-world-male-robot-on-computer.png" />
-		</div>
+	<div tw="w-[1200px] h-[630px] flex flex-col items-center justify-center text-center">
+        <div
+          tw="bg w-full h-full flex"
+          style={{
+            backgroundImage:
+              "linear-gradient(90deg, #101010, rgba(0, 0, 0, 0.83)), url('/noise.svg'),
+			  linear-gradient(#b3fc03 1px, transparent 0), linear-gradient(90deg, #b3fc03 1px, #000 0)",
+          }}
+        >
+          <div tw="flex flex-col text-3xl tracking-tight text-gray-300 w-full items-center h-full justify-center text-center">
+            <h1
+              tw="text-white text-7xl"
+              style={{
+                color: "transparent",
+                paddingLeft: "12rem",
+                paddingRight: "12rem",
+                backgroundImage:
+                  "linear-gradient(to top, rgba(244, 244,  245, .5), rgba(255,255,255,1))",
+                backgroundClip: "text",
+              }}
+            >
+              AbdulTech
+            </h1>
+            <p tw="mt-4 font-bold">build . ship . live life</p>
+          </div>
+        </div>
+      </div>
 	`;
 
 	return new ImageResponse(template, {
