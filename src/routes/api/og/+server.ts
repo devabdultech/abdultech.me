c655887b252c7c6071274acb6f9ba0ab065f8aa6
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-11-12 23:33:02 +0100

    ✨ refactor OG image server endpoint.

--- src/routes/api/og/+server.ts
+++ src/routes/api/og/+server.ts
@@ -3,25 +3,15 @@ import { html } from 'satori-html';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
-	let imagePath;
-
-	if (import.meta.env.MODE === 'development') {
-		// In development, use the localhost image path
-		imagePath = 'http://localhost:5173';
-	} else {
-		imagePath = 'https://abdultech-me.vercel.app';
-	}
-
 	const template = html`
 	<div tw="w-[1200px] h-[630px] flex flex-col items-center justify-center text-center">
-        <div
-          tw="bg w-full h-full flex"
-          style={{
-            backgroundImage:
-              "linear-gradient(90deg, #101010, rgba(0, 0, 0, 0.83)), url('/noise.svg'),
-			  linear-gradient(#b3fc03 1px, transparent 0), linear-gradient(90deg, #b3fc03 1px, #000 0)",
-          }}
-        >
+  <div
+  tw="bg-black w-full h-full flex"
+  style={{
+    backgroundImage:
+      "linear-gradient(to top right, rgba(24,24,27,.5), rgba(63,63,70,.3))",
+  }}
+>
           <div tw="flex flex-col text-3xl tracking-tight text-gray-300 w-full items-center h-full justify-center text-center">
             <h1
               tw="text-white text-7xl"
@@ -43,7 +33,7 @@ export const GET: RequestHandler = async () => {
 	`;
 
 	return new ImageResponse(template, {
-		height: 400,
-		width: 600
+		height: 630,
+		width: 1200
 	});
 };
